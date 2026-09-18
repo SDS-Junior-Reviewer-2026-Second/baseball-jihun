4d8e0709fbf98f3dfc48711a4e66f747ae82f0f0
Jihun Park <bjhbjh0490@gmail.com>
2026-09-18 13:06:44 +0900

    [feature] 73p TDD Green 8

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -59,12 +59,14 @@ public class GameTest {
 
 	@Test
 	public void 볼만_있을_경우_0_strike_1_ball() {
-
+		generateQuestion("123");
+		assertMatchNumber(game.guess("718"),false, 0, 1);
 	}
 
 	@Test
 	public void 볼과_스트라이크가_함께_있을_경우_1_strike_1_ball() {
-
+		generateQuestion("123");
+		assertMatchNumber(game.guess("713"),false, 1, 1);
 	}
 
 	private static void assertMatchNumber(GuessResult result, boolean solved, int strikes, int balls) {
