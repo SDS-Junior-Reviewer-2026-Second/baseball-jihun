bac642c0b77f2d3650e5462abfcaf302d570419a
Jihun Park <bjhbjh0490@gmail.com>
2026-09-18 11:37:54 +0900

    [feature] 68p TDD Blue 6

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,6 +1,5 @@
 package baseball;
 
-import org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -38,38 +37,39 @@ public class GameTest {
 	
 	@Test
 	public void 숫자_세개가_전부_일치_할_경우_3_strike() {
-		game.question = "123";
-		GuessResult result = game.guess("123");
-
-		assertThat(result).isNotNull();
-		assertThat(result.isSolved()).isEqualTo(true);
-		assertThat(result.getStrikes()).isEqualTo(3);
-		assertThat(result.getBalls()).isEqualTo(0);
+		generateQuestion("123");
+		assertMatchNumber(game.guess("123"), true, 3, 0);
 	}
-	
+
 	@Test
 	public void 숫자_세개가_전부_일치_하지_않을_경우_0_strike_0_ball() {
-		game.question = "123";
-		GuessResult result = game.guess("456");
-
-		assertThat(result).isNotNull();
-		assertThat(result.isSolved()).isEqualTo(false);
-		assertThat(result.getStrikes()).isEqualTo(0);
-		assertThat(result.getBalls()).isEqualTo(0);
+		generateQuestion("123");
+		assertMatchNumber(game.guess("456"), false, 0, 0);
 	}
-	
+
+	private void generateQuestion(String questionNumber) {
+		game.question = questionNumber;
+	}
+
 	@Test
 	public void 스트라이크만_있을_경우_1_strike_0_ball() {
 
 	}
-	
+
 	@Test
 	public void 볼만_있을_경우_0_strike_1_ball() {
 
 	}
-	
+
 	@Test
 	public void 볼과_스트라이크가_함께_있을_경우_1_strike_1_ball() {
 
+	}
+
+	private static void assertMatchNumber(GuessResult result, boolean solved, int strikes, int balls) {
+		assertThat(result).isNotNull();
+		assertThat(result.isSolved()).isEqualTo(solved);
+		assertThat(result.getStrikes()).isEqualTo(strikes);
+		assertThat(result.getBalls()).isEqualTo(balls);
 	}
 }
