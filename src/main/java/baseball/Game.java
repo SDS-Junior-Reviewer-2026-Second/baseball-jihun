39d7704ad70827f6015eea0fed7dfc52fc189d75
Jihun Park <bjhbjh0490@gmail.com>
2026-09-18 11:24:37 +0900

    [feature] 51p TDD Blue 4

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,7 +3,11 @@ package baseball;
 public class Game {
 
     public void guess(String guessNumber) {
-        if(guessNumber==null){
+        assertIllegalArgument(guessNumber);
+    }
+
+    private static void assertIllegalArgument(String guessNumber) {
+        if(guessNumber ==null){
             throw new IllegalArgumentException();
         }
         if(guessNumber.length() !=3){
@@ -16,10 +20,14 @@ public class Game {
             }
         }
 
-        if(guessNumber.charAt(0) == guessNumber.charAt(1)
-        ||guessNumber.charAt(1)==guessNumber.charAt(2)
-        ||guessNumber.charAt(2) == guessNumber.charAt(0)){
+        if(isDuplicatedNumber(guessNumber)){
             throw new IllegalArgumentException();
         }
+    }
+
+    private static boolean isDuplicatedNumber(String guessNumber) {
+        return guessNumber.charAt(0) == guessNumber.charAt(1)
+                || guessNumber.charAt(1) == guessNumber.charAt(2)
+                || guessNumber.charAt(2) == guessNumber.charAt(0);
     }
 }
